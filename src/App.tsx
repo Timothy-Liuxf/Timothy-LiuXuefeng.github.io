09afc3e8cc48e264eab4868486dbb4912de9afea
Timothy Liu <liuxf19@163.com>
2022-11-16 15:23:04 +0800

    feat: :sparkles: change page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profile from "./profile.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <a href="https://github.com/Timothy-Liuxf">
+          <img src={profile} className="App-logo" alt="profile" />
+        </a>
+        <p>Timothy Liu's Home Page (Deprecated)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Timothy-Liuxf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View GitHub Profile
         </a>
       </header>
     </div>
